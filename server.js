2dd6593f4dcdb354d972720d53d2bc9a1f937b7b
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-12 17:56:06 +0530

    connect neondb, prisma, and schemas

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv/config");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.use("/", (req, res) => {
-  res.send("Server is Live");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Server is Live.");
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on PORT", PORT);
+  console.log("Server is running...");
 });
