b4921e078456a9ebed75e2f02822cbd11e2a39ff
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-13 21:43:00 +0530

    fix cookie issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -206,7 +206,7 @@ const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -227,7 +227,7 @@ const logoutUser = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     });
 
     return res.status(200).json({
